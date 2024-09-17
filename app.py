5f219135750a0a52d96662ec29211340045c673f
Muminur <muminur.rashid@student.education.wa.edu.au>
2024-09-17 21:25:02 +0800

    Initialise the SCMS app (app.py)

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ from models import init_db
 import views
 
 app = Flask(__name__)
+
+init_db()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
